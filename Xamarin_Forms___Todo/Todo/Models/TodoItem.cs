22fde599ec78daa7b890075419504a3677b991c9
rsanders001 <rsanders2k@gmail.com>
2020-04-22 13:51:59 -0500

    Homework 11 Commit

---
+++ Xamarin_Forms___Todo/Todo/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace Todo
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Joke { get; set; }
+       
+    }
+}
+
---
+++ Xamarin_Forms___Todo/Todo/Views/TodoItemPageCS.cs
@@ -0,0 +1,42 @@
+﻿using Xamarin.Forms;
+
+namespace Todo
+{
+    public class TodoItemPageCS : ContentPage
+    {
+        public TodoItemPageCS()
+        {
+            Title = "Jokes";
+
+            var jokeEntry = new Entry();
+            jokeEntry.SetBinding(Entry.TextProperty, "Joke");
+
+            var doneSwitch = new Switch();
+            doneSwitch.SetBinding(Switch.IsToggledProperty, "Done");
+
+            var saveButton = new Button { Text = "Save" };
+            saveButton.Clicked += async (sender, e) =>
+            {
+                var todoItem = (TodoItem)BindingContext;
+                await App.Database.SaveItemAsync(todoItem);
+                await Navigation.PopAsync();
+            };
+
+            
+
+            Content = new StackLayout
+            {
+                Margin = new Thickness(20),
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                Children =
+                {
+                    new Label { Text = "Joke" },
+                    jokeEntry,
+                    new Label { Text = "Done" },
+                    doneSwitch,
+                    saveButton,
+                }
+            };
+        }
+    }
+}
